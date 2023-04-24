84856b4222850b168a168acbf5701f34f2f64e38
Manan-Anghan <126051337+Manan-Anghan@users.noreply.github.com>
2023-04-24 06:22:28 -0700

    Update pr2_1.cpp

--- pr 2/pr2_1.cpp
+++ pr 2/pr2_1.cpp
@@ -1,130 +1,180 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+    #include<string.h>
+    using namespace std;
 
-using namespace std;
+    class bill{
+        private:
+            string item_name;
+            int quantity;
+            double tax;
+            int discount;
+        public:
+            int item_number;
 
-class Train {
-	
-public:
-    int t_Number;
-    string t_Name,Source,t_Destination,t_Time,
+            void password()
+            {
+                char uName[100] , pass[100] , cp[100];
+                int lw=0,dot=0,dg=0,up=0,sc=0,sp=0,un=0,i,len,n,m;
 
-    Train() {}
+                cout << "Enter Your Username : ";
+                cin >> uName;
 
-    Train(int t_Number, string t_Name, string Source, string t_Destination, string t_Time) {
-        this->t_Number = t_Number;
-        this->t_Name = t_Name;
-        this->Source = Source;
-        this->t_Destination = t_Destination;
-        this->t_Time = t_Time;
-    }
+                for(i=0 ; i<strlen(uName) ; i++)
+                {
+                    if(uName[i]>='A' && uName[i]<='Z')
+                    {
+                        up++;
+                    }
+                    else if(uName[i]>='a' && uName[i]<='z')
+                    {
+                        lw++;
+                    }
+                    else if(uName[i]>='0' && uName[i]<='9')
+                    {
+                        dg++;
+                    }
+                    else if(uName[i]=='_')
+                    {
+                        un++;
+                    }
+                    else if(uName[i]=='.')
+                    {
+                        dot++;
+                    }
+                    else
+                    {
+                        sc++;
+                    }
+                }
+                if(up>=0 && lw>=0 && dg>=0 && un==2 && dot==0 && sc==0)
+                {
+                    cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    cout << "Valid UserName ..." << endl;
+                    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                }	
+                else
+                {
+                    cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    cout << "Invalid UserName..." << endl;
+                    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                }
 
-    void displayTrainDetails() {
-    	cout <<"\n\n==================================================\n" ;
-	    cout << "\n\t-----------------------------------------";
-		cout << "\n\t|  Train number\t : "<<t_Number<<"\t\t\t|";
-		cout << "\n\t|  Train name\t : "<<t_Name<<"\t\t\t|";
-		cout << "\n\t|  Arriving at\t : "<<t_Time<<"\t\t\t|";
-		cout << "\n\t|  Route\t : From "<<Source<<" to "<<t_Destination<<"\t|";
-	    cout << "\n\t-----------------------------------------";
-	    cout <<"\n\n==================================================\n" ;
-    }
-};
+                if(lw>0 && up>0 && dot==0 && sc==0 && strlen(pass)>=8);
+                {
+                    cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    cout << "Confirm Username : " << uName << endl;
+                    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    cout << endl <<"Enter Your Password : ";
+                    cin >> pass;
 
-Train TRAINS[15];
+                    len = strlen(pass);
 
-void addTrain() {
-    static int count = 0;
-    if (count >= 15) 
-	{
-        cout << "Maximum number of Trains added!" << endl;
-        return;
-    }
-    int t_Number;
-    string t_Name, Source, t_Destination, t_Time;
-    
-	cout<<"You are adding the train details now : \n\n";
-	
-    cout << "\tEnter train number: ";
-    cin >> t_Number;
+                    if(lw>=0 && up>=0 && dot==0 && sc==0 && strlen(pass)>=8)
+                    {
+                        cout << "Confirm Your Password : ";
+                        cin >> cp;
+                        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                        strlwr(pass);
+                        strlwr(cp);
 
-    cout << "\tEnter train name: ";
-    cin >> t_Name;
+                        n = strcmp(pass,cp);
 
-    cout << "\tEnter source: ";
-    cin >> Source;
+                        if(n==0)
+                        {
+                            cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                            cout << "You Have a Login Sucessfull.." << endl;
+                            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                        }
+                        else
+                        {
+                            cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                            cout << "Your Password is Incorrect.." << endl;
+                            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                        cout << "Incorrect Password.." << endl;
+                        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                    }
+                }
+            }
 
-    cout << "\tEnter destination: ";
-    cin >> t_Destination;
+            void setData()
+            {
+                cout << endl << "Enter Item Number : ";
+                cin >> item_number;
+                cout << "Enter Item Name : ";
+                cin >> item_name;
+                cout << "Enter Item Quantity : ";
+                cin >> quantity;
+                cout << "Enter Item Tax : ";
+                cin >> tax;
+                cout << "Enter Item Discount: ";
+                cin >> discount;
+            }	
 
-    cout << "\tEnter train time: ";
-    cin >> t_Time;
+            void getData()
+            {
+                cout << endl << "I_no.\t : " << item_number << endl
+                            << "I_name\t : " << item_name << endl
+                            << "I_Quan\t : " << quantity << endl
+                            << "I_tax\t : " << tax << endl
+                            << "I_dis\t : " << discount << endl ;
+            }
 
-	cout << "\n\tThe following train has been added successfully!";
-	cout << "\n\t-----------------------------------------";
-	cout << "\n\t|  Train number\t : "<<t_Number<<"\t\t\t|";
-	cout << "\n\t|  Train name\t : "<<t_Name<<"\t\t\t|";
-	cout << "\n\t|  Arriving at\t : "<<t_Time<<"\t\t\t|";
-	cout << "\n\t|  Route\t : From "<<Source<<" to "<<t_Destination<<"\t|";
-    cout << "\n\t-----------------------------------------";
-    
-    cout <<"\n\n==================================================\n" ;
-    		
-    TRAINS[count] = Train(t_Number, t_Name, Source, t_Destination, t_Time);
-    count++;
-}
+            static void header()
+            {
+                cout << endl << "I_no\tI_name\tI_Quan\tI_tax\tI_dis\n======= ======= ======= ======= =======\n";
+            }
 
-void displayTrainByNumber() {
-    int t_Number;
-    int found = 0;
+            void getDataInTable()
+            {
+                cout << item_number << "\t" << item_name << "\t" << quantity << "\t" << tax << "\t" << discount << endl;
+            }
 
-    cout << "Enter train number to search: ";
-    cin >> t_Number;
 
-    for (int i = 0; i < 15; i++) {
-        if (TRAINS[i].t_Number == t_Number) {
-            TRAINS[i].displayTrainDetails();
-            found = 1;
-            break;
+    };
+
+    int main()
+    {
+
+        int n,i,j;
+
+        bill b1[50];
+        b1[0].password();
+
+        cout << endl << "Enter Number of Bill : ";
+        cin >> n;
+
+        for(int i=0 ; i<n ; i++)
+        {
+            b1[i].setData();
         }
-    }
 
-    if (found==0) {
-        cout << "\noops! Train not found!\n" << endl;
-    }
-}
-
-int main() {
-    int choice;
-    do {
-        cout << "\n1. Add train" << endl;
-        cout << "2. Search train by number" << endl;
-        cout << "3. Exit" << endl;
-        cout << "\nEnter your choice: ";
-        cin >> choice;
-		cout <<"\n==================================================\n" ;
-        switch (choice) 
-		{
-        	
-        case 1:
-            addTrain();
-            break;
-            
-        case 2:
-            displayTrainByNumber();
-            break;
-            
-        case 3:
-            cout << "Exiting..." << endl;
-            break;
-            
-        default:
-            cout << "Invalid choice! Try again." << endl;
-            break;
+        bill temp; 
+        for(i=0 ; i<n ; i++)
+        {
+            for(j=i+1 ; j<n ; j++)
+            {
+                if(b1[i].item_number > b1[j].item_number)
+                {
+                    temp = b1[i];
+                    b1[i] = b1[j];
+                    b1[j] = temp;
+                }
+            }
         }
+
+
+
+        bill::header();
+
+        for(int i=0 ; i<n ; i++)
+        {
+            b1[i].getDataInTable();
+        }
+
     }
-	 while (choice != 3);
-
-    return 0;
-}
-
